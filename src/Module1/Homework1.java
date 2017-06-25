8009c660dc10cd928d82e30d74fc255664516696
Дмитрий Сапатов < >
2017-06-25 10:52:08 +0300

    First commit

---
+++ src/Module1/Homework1.java
@@ -0,0 +1,11 @@
+package Module1;
+
+/**
+ * Created by night on 25.06.2017.
+ */
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("Как я проведу лето 2017");
+        System.out.println("Искренне надеюсь поехать на море.");
+    }
+}
